731e3505fe84e471cadf7c0790e368efb57824be
Himanshu Sahdev aka CunningLearner <sahdev.himan@gmail.com>
2020-07-09 18:31:30 +0000

    Avoid replication of processes
    As we are always increasing the size of vector replicating the values after
    every second, the program will use the whole memory of the system in a few
    minutes. This commit resolves this problem by clearing the vector in start
    of the function.
    
    Signed-off-by: Himanshu Sahdev aka CunningLearner <sahdev.himan@gmail.com>

--- System_Monitor/src/system.cpp
+++ System_Monitor/src/system.cpp
@@ -20,7 +20,7 @@ Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() {
-//   std::sort(processes_.begin(), processes_.end());
+  processes_.clear();
   vector<int> pids = LinuxParser::Pids();
   for(auto pid : pids){
     processes_.emplace_back(Process(pid));
